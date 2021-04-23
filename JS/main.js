a56c3de6d46124bf88e734ecb764e48d2bffa747
Alex <80860409+akelee@users.noreply.github.com>
2021-04-23 21:28:35 +0200

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,45 @@
+let sliderImages = document.querySelectorAll('.slide'), arrowLeft = document.querySelector('#arrow-left'),
+arrowRight = document.querySelector('#arrow-right'),
+current = 0; //current image
+
+//clear out img eah time to display none when sliding
+function reset() {
+    for(let i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = 'none';
+    }
+}
+//initialiser
+function startSlide() {
+    reset();
+    sliderImages[0].style.display = 'block';
+}
+//show previous
+function slideLeft() {
+    reset();
+    sliderImages[current -1].style.display ='block';
+    current--;
+}
+
+//show next
+function slideRight() {
+    reset();
+    sliderImages[current + 1].style.display = 'block';
+    current++;
+}
+//Left arrow click
+arrowLeft.addEventListener('click', function() {
+    if(current === 0){
+        current = sliderImages.length;
+    }
+    slideLeft();
+});
+//Right arrow click
+arrowRight.addEventListener('click', function() {
+    if(current === sliderImages.length - 1) {
+        current = -1
+
+    }
+    slideRight();
+});
+
+startSlide();
